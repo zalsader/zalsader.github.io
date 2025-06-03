704bceffbdc5d86fa2e45f0efba8138c6db1ca2e
zalsader <zalsader@hotmail.com>
2025-06-03 05:28:50 +0000

    Deploying to gh-pages from @ zalsader/zalsader.github.io@0fa0d6e92227920543e26bbb22a3ee7bef806242 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-welcome-to-my-blog",
+        },{id: "post-serverless-storage-with-knative-and-ceph",
+      
+        title: "Serverless Storage with Knative and Ceph",
+      
+      description: "How to use Ceph for storage in serverless functions using knative",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/ceph-knative/";
+        
+      },
+    },{id: "post-welcome-to-my-blog",
       
         title: "Welcome to my blog!",
       
